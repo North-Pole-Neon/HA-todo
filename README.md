989e65d29486c211e40babf0070922512ec252bf
Tyler Moen <tyler.moen@earthlink.net>
2021-12-10 11:54:52 -0600

    Starting dev on todo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# HA-todo
+# HA-todo | *Home Adiutor Todo Module*
+
+HA-todo is the official todo module for Home Adiutor.
+
+## Features
+- Keep track of tasks
+- Organize tasks in labels
+- Remind me of tasks at certain times
--- src/main/app.py
+++ src/main/app.py
@@ -1,17 +1,66 @@
+# import packages
 
-def todoRead():
-    pass
+class ha_todo():
+    name = 'Todo'
+    desc = 'Simple todo tracker'
+    author = 'Tyler Moen'
+    version = '0.01'
+    mod_standard = '1.0'
+    listeners = ['timer', 'voice']
+    clock_req = True
+    clock_jobs = [] 
 
-def todoAdd():
-    pass
+    def __init__():
+        """
+        This function should be avoided for most tasks and start() should be used instead
+        """
+        pass
 
-def todoFinish():
-    pass
+    def scheduler_com():
+        """
+        This is what will be used to handle any Scheduler communication
+        """
+        pass
 
-def todoRemove():
-    pass
+    def install():
+        """
+        Any post install tasks will be placed here
+        """
+        pass
 
-def todoClear():
-    pass
+
+    def start():
+        """
+        This is where your class is started when activated. Try to keep any required functions nested in this function. 
+        """
+        pass
+
+    def end():
+        """
+        This is where you would prep disabling the application
+        """
+        pass
+
+    def instanced(self, call):
+        """
+        This is what is going to run when an event is triggered based on the input
+        """
+        pass
+
+
+    def todoRead():
+        pass
+
+    def todoAdd():
+        pass
+
+    def todoFinish():
+        pass
+
+    def todoRemove():
+        pass
+
+    def todoClear():
+        pass
 
 
